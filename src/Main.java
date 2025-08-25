b42f7325f9847ae95c2a683011f0a4e9aee1ffee
juanriosw3c <riosjuanjose1998@gmail.com>
2025-08-25 20:06:13 -0300

    v2

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,9 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		JOptionPane.showMessageDialog(null, 
-				"Hola soy Emilio");
-		
-		
+
+		JOptionPane.showMessageDialog(null,
+				"Hola soy Juan");
+
 	}
 }
